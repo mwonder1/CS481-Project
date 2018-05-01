895a54a1ab4bab79efb391a696f70af18e875a7d
mwonder1 <37061388+mwonder1@users.noreply.github.com>
2018-05-01 12:09:37 -0400

    Preferences WIP

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,11 +1,14 @@
 package main.java;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -17,9 +20,39 @@ public class MainApp extends Application {
 		launch(args);
 	}
 
+	public String getDestination() {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		String filePath = prefs.get("destination", null);
+		if (filePath != null)
+			return filePath;
+		else
+			return null;
+	}
+
+	public void setDestination(String destination) {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		if (destination != null) {
+			prefs.put("destination", destination);
+
+		} else {
+			prefs.remove("destination");
+
+		}
+	}
+
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		MainApp.primaryStage = primaryStage;
+		if (getDestination() == null) {
+
+			FileChooser fileChooser = new FileChooser();
+			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("SER", "*.ser");
+			fileChooser.getExtensionFilters().add(extFilter);
+			File file = fileChooser.showSaveDialog(MainApp.primaryStage);
+			String destination = file.getAbsolutePath();
+
+			setDestination(destination);
+		}
 		startScreen();
 	}
 
