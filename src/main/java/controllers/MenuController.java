a57aa5115269d3cf88b705930f3aedd2a50a7eed
mwonder1 <37061388+mwonder1@users.noreply.github.com>
2018-05-08 13:49:46 -0400

    WIP UI Menu Bar

---
+++ src/main/java/controllers/MenuController.java
@@ -0,0 +1,30 @@
+package main.java.controllers;
+
+public class MenuController {
+
+	public static void aboutBtn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void genBtn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void importBtn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void saveBtn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void viewBtn() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
